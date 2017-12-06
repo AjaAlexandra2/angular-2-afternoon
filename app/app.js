a5018055f8c0c0b10057941a75d8991d4fb235ba
Ajaalexandra <ajacederberg@yahoo.com>
2017-12-06 15:07:03 -0600

    finished devmtn travels

--- app/app.js
+++ app/app.js
@@ -1,10 +1,40 @@
-angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider, $urlRouterProvider ) {
-  $stateProvider
-    .state('home',{
-      url:'/',
-      templateUrl: "app/about/aboutTmpl.html"
-    });
+angular
+  .module("devmtnTravel", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "app/about/aboutTmpl.html"
+      })
 
-  $urlRouterProvider
-    .otherwise('/');
-});
+      .state("packages", {
+        url: "/packages/:country",
+        templateUrl: "app/packages/packagesTmpl.html",
+        controller: "packagesCtrl"
+      })
+
+      .state("locations", {
+        url: "/locations",
+        templateUrl: "app/locations/locationsTmpl.html",
+        controller: "locationsCtrl"
+      })
+
+      .state("booked", {
+        url: "/booked/:id",
+        templateUrl: "app/booked/bookedTmpl.html",
+        controller: "bookedCtrl"
+      })
+
+      .state("adventurers", {
+        url: "/adventurers",
+        templateUrl: "app/about/adventurers/adventurersTmpl.html",
+        parent: "home"
+      })
+      .state("contact", {
+        url: "/contact",
+        templateUrl: "app/about/contact/contactTmpl.html",
+        parent: "home"
+      });
+
+    $urlRouterProvider.otherwise("/");
+  });
